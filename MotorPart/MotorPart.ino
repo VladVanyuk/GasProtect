86883fb6e4efab1cb558f21f854c0cb376c4a53a
Vladislav Vanyuk <vladislavvanyuk@gmail.com>
2024-04-25 22:52:23 +0300

    SensorPart style rework and optimization: 1) Chenged timers logic not to use delay(); 2) new functions: initSerial, initRadio, blinkLed, getSensorValueAvg, radioRecv. 3) Added new defines and renamed old: SERIAL_SPD, LED_PIN, LAMP_PIN, SENSOR_PIN, LOOP_WAIT_TIME, SLEEP_TIME_SEC, READ_SENSOR_TIMEOUT, THRESHOLD_SENSOR_VAL. 4) predefined settings usage: cahanged <if> to #<#if> 5) Moved constant strings to program memory.
    MotorPart fixed names of used pins, added new defines, renamed function <Blink> to <blinkLed>.

--- MotorPart/MotorPart.ino
+++ MotorPart/MotorPart.ino
@@ -1,64 +1,53 @@
 #include <LowPower.h>
-
 #include <RFM69.h>
 #include <SPI.h>
 
-// Addresses for this node. CHANGE THESE FOR EACH NODE!
+#define SERIAL_SPD 9600
 
+// Addresses for this node. CHANGE THESE FOR EACH NODE!
 #define NETWORKID 0  // Must be the same for all nodes
 #define MYNODEID 2   // My node ID
 #define TONODEID 1   // Destination node ID
 
 // RFM69 frequency, uncomment the frequency of your module:
-
 #define FREQUENCY RF69_868MHZ
-
 #define ENCRYPT true                   // Set to "true" to use encryption
 #define ENCRYPTKEY "TOPSECRETPASSWRD"  // Use the same 16-byte key on all nodes
+#define USEACK true  // Use ACKnowledge when sending messages (or not):
 
-// Use ACKnowledge when sending messages (or not):
+#define LOOP_WAIT_TIME 30000
 
-#define USEACK true  // Request ACKs or not
+#define LED_PIN 9  // LED_PIN positive pin
+const uint8_t led_pin = 13;
+const uint8_t motor_pin1 = 6;
+const uint8_t buttonStart_pin = 7;
+const uint8_t buttonEnd_pin = 8;
 
-// Packet sent/received indicator LED (optional):
-
-#define LED 9  // LED positive pin
-#define GND 8  // LED ground pin
+float sensorValue = 0.0;
+float sensorVolts = 0.0;
 
 // Create a library object for our RFM69HCW module:
-
 RFM69 radio;
 
-int buttonStart = 7;
-int buttonEnd = 8;
-int led_pin = 13;
-int motor_pin1 = 6;
-byte speed = 50;
-float sensorValue;
-float sensorVolts;
-
-int counter = 0;
-
 void setup() {
-  // Open a serial port so we can send keystrokes to the module:
+  Serial.begin(SERIAL_SPD);
 
-  Serial.begin(9600);
   Serial.print("Node ");
   Serial.print(MYNODEID, DEC);
   Serial.println(" ready");
 
-  pinMode(buttonStart, INPUT_PULLUP);
-  pinMode(buttonEnd, INPUT_PULLUP);
+  pinMode(buttonStart_pin, INPUT_PULLUP);
+  pinMode(buttonStart_pin, INPUT_PULLUP);
   pinMode(motor_pin1, OUTPUT);
   pinMode(led_pin, OUTPUT);
+
   radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
   radio.setHighPower();  // Always use this for RFM69HCW
 
   // Turn on encryption if desired:
-
-  if (ENCRYPT) {
+#if (ENCRYPT==true)
     radio.encrypt(ENCRYPTKEY);
-  }
+#endif
 }
 
 void loop() {
@@ -69,19 +58,17 @@ void loop() {
     }
     unsigned long startTime = millis();
 
-    while (millis() - startTime < 30000) {
+    while (millis() - startTime < LOOP_WAIT_TIME) {
       // Set up a "buffer" for characters that we'll send:
       static char sendbuffer[62];
       static int sendlength = 0;
 
       // SENDING
-
       // In this section, we'll gather serial characters and
       // send them to the other node if we (1) get a carriage return,
       // or (2) the buffer is full (61 characters).
 
       // If there is any serial input, add it to the buffer:
-
       if (Serial.available() > 0) {
         char input = Serial.read();
 
@@ -92,12 +79,8 @@ void loop() {
         }
 
         // If the input is a carriage return, or the buffer is full:
-
         if ((input == '\r') || (sendlength == 61))  // CR or buffer full
         {
-          // Send the packet!
-
-
           Serial.print("sending to node ");
           Serial.print(TONODEID, DEC);
           Serial.print(", message [");
@@ -114,33 +97,27 @@ void loop() {
           {
             radio.send(TONODEID, sendbuffer, sendlength);
           }
-
           sendlength = 0;  // reset the packet
-          Blink(LED, 10);
+          blinkLed(LED_PIN, 10);
         }
       }
 
       // RECEIVING
-
       // In this section, we'll check with the RFM69HCW to see
       // if it has received any packets:
       if (radio.receiveDone())  // Got one!
       {
-        // Print out the information:
-
         Serial.print("received from node ");
         Serial.print(radio.SENDERID, DEC);
         Serial.print(", message [");
 
         // The actual message is contained in the DATA array,
-        // and is DATALEN bytes in size:
-
+        // and is DATALEN bytes in size
         for (byte i = 0; i < radio.DATALEN; i++)
           Serial.print((char)radio.DATA[i]);
 
         // RSSI is the "Receive Signal Strength Indicator",
         // smaller numbers mean higher power.
-
         Serial.print("], RSSI ");
         Serial.println(radio.RSSI);
 
@@ -155,12 +132,11 @@ void loop() {
           Serial.println("TRUE");
 
           while (true) {
-
-            if (digitalRead(buttonStart) == HIGH) {
+            if (digitalRead(buttonStart_pin) == HIGH) {
               Serial.println("First");
               digitalWrite(motor_pin1, HIGH);
               
-            } else if (digitalRead(buttonEnd) == HIGH) {
+            } else if (digitalRead(buttonStart_pin) == HIGH) {
               Serial.println("Third");
               digitalWrite(motor_pin1, LOW);
               break;
@@ -170,20 +146,17 @@ void loop() {
 
           Serial.println("FALSE");
         }
-
-    // Send an ACK if requested.
-    // (You don't need this code if you're not using ACKs.)
       } else {
         delay(50);
       }
 
       // Send an ACK if requested.
-
+      // (You don't need this code if you're not using ACKs.)
       if (radio.ACKRequested()) {
         radio.sendACK();
         Serial.println("ACK sent");
       }
-      // Blink(LED, 10);
+      // blinkLed(LED_PIN, 10);
     }
     Serial.println("sleep");
     sleepForSeconds(24);
@@ -191,8 +164,8 @@ void loop() {
   }
 }
 
-void Blink(byte PIN, int DELAY_MS)
-// Blink an LED for a given number of ms
+// Blink an LED_PIN for a given number of ms
+void blinkLed(uint8_t PIN, int DELAY_MS)
 {
   digitalWrite(PIN, HIGH);
   delay(DELAY_MS);
--- SensorPart/SensorPart.ino
+++ SensorPart/SensorPart.ino
@@ -1,175 +1,180 @@
+#include <Arduino.h>
 #include <LowPower.h>
-
 #include <RFM69.h>
 #include <SPI.h>
 
-// Addresses for this node. CHANGE THESE FOR EACH NODE!
+#define SERIAL_SPD 9600
+#define LED_PIN 9      // led positive pin
+#define LAMP_PIN 13    // lamp pin
+#define SENSOR_PIN 0 
 
+// Addresses for this node. CHANGE THESE FOR EACH NODE
 #define NETWORKID 0  // Must be the same for all nodes
 #define MYNODEID 1   // My node ID
 #define TONODEID 2   // Destination node ID
 
 // RFM69 frequency, uncomment the frequency of your module:
-
 #define FREQUENCY RF69_868MHZ
-
-// AES encryption (or not):
-
 #define ENCRYPT true                   // Set to "true" to use encryption
 #define ENCRYPTKEY "TOPSECRETPASSWRD"  // Use the same 16-byte key on all nodes
 
 // Use ACKnowledge when sending messages (or not):
-
-#define USEACK true  // Request ACKs or not
+#define USEACK true  
 #define ACK_WAIT_TIME 1000
-// Packet sent/received indicator LED (optional):
+#define LOOP_WAIT_TIME 30000
+#define SLEEP_TIME_SEC 24
+#define READ_SENSOR_TIMEOUT 100 
 
-#define LED 9  // LED positive pin
-#define GND 8  // LED ground pin
-
-// Create a library object for our RFM69HCW module:
-
-RFM69 radio;
-double sensorValue;  // changed to double to increase precision
-double sensorVolts;
-const int lamp = 13;  // this should be #define, not const int
-
-int counter = 0;
-/* Messaging */
 #define BUFFER_SIZE 25
+#define THRESHOLD_SENSOR_VAL 600  // threshold value for sensor 
+
+float sensorValue = 0.0;  // changed to double to increase precision
 char sendBuffer[BUFFER_SIZE] = {};
 uint8_t sendLength = 0;  // using 8-bit unsigned integer to reduce RAM usage (max value 255)
-void radioSend(double value);
+const char message_start[] PROGMEM = "yes";
+uint32_t startTime =0;
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial)
+RFM69 radio;
+
+void initSerial() {
+  Serial.begin(SERIAL_SPD);
+  while (!Serial) {
     ;
-  Serial.print("Node ");
-  Serial.print(MYNODEID, DEC);
-  Serial.println(" ready");
+  }
+}
 
+void initRadio() {
   // Initialize the RFM69HCW:
+  Serial.print(F("Node "));
+  Serial.print(MYNODEID, DEC);
+  Serial.println(F(" ready"));
+
   // radio.setCS(10);  //uncomment this if using Pro Micro
   radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
   radio.setHighPower();  // Always use this for RFM69HCW
 
-  // Turn on encryption if desired:
-
-  if (ENCRYPT)
+#if (ENCRYPT==true)
     radio.encrypt(ENCRYPTKEY);
-  Serial.begin(9600);
-  delay(20000);
-  pinMode(lamp, OUTPUT);
+#endif
 }
 
-void loop() {
-  char message[] = "yes";
-  radio.sendWithRetry(2, message, sizeof(message), 3, ACK_WAIT_TIME);
-  unsigned long startTime = millis();
 
-  while (millis() - startTime < 30000) {
-
-    for (int i = 0; i < 100; i++) {
-      sensorValue = sensorValue + analogRead(0);
-    }
-
-    sensorValue = sensorValue / 100;
-    sensorVolts = sensorValue / 1024 * 5.0;
-    Serial.println(sensorValue);
-    Serial.println(sensorVolts);
-    delay(100);
-
-    // if sensor value is higher than some threshold, send message
-    if (sensorValue > 600) {
-      digitalWrite(lamp, LOW);
-      radioSend(sensorValue);  // sending
-    } else {
-      digitalWrite(lamp, HIGH);
-    }
-
-
-    // SENDING
-
-    // In this section, we'll gather serial characters and
-    // send them to the other node if we (1) get a carriage return,
-    // or (2) the buffer is full (61 characters).
-
-    // If there is any serial input, add it to the buffer:
-
-    // RECEIVING
-
-    // In this section, we'll check with the RFM69HCW to see
-    // if it has received any packets:
-
-    if (radio.receiveDone())  // Got one!
-    {
-      // Print out the information:
-
-      Serial.print("received from node ");
-      Serial.print(radio.SENDERID, DEC);
-      Serial.print(", message [");
-
-      // The actual message is contained in the DATA array,
-      // and is DATALEN bytes in size:
-
-      for (byte i = 0; i < radio.DATALEN; i++)
-        Serial.print((char)radio.DATA[i]);
-
-      // something like command handler
-      /* command "read"
-     * should send sensor value 
-     */
-      if (!strcmp(radio.DATA, "read"))  // not sure if this will work (radio.DATA == "read")
-      {
-        radioSend(sensorValue);
-      }
-      // RSSI is the "Receive Signal Strength Indicator",
-      // smaller numbers mean higher power.
-
-      Serial.print("], RSSI ");
-      Serial.println(radio.RSSI);
-
-      // Send an ACK if requested.
-      // (You don't need this code if you're not using ACKs.)
-
-      if (radio.ACKRequested()) {
-        radio.sendACK();
-        Serial.println("ACK sent");
-      }
-      Blink(LED, 10);
-    }
-  }
-  sleepForSeconds(24);
+// Blink an LED_PIN for a given number of ms
+void blinkLed(uint8_t PIN, int DELAY_MS)
+{
+  digitalWrite(PIN, HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN, LOW);
 }
 
 void sleepForSeconds(unsigned int seconds) {
   unsigned int sleepCycles = seconds / 8;
-
   for (unsigned int i = 0; i < sleepCycles; i++) {
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   }
 }
 
-void radioSend(double value) {
-  memset(sendBuffer, 0, BUFFER_SIZE);  // clear sending buffer
+float getSensorValueAvg() {
+  float sensorValueAvg=0;
+  for (uint8_t i = 0; i < 100; i++) {
+    sensorValueAvg += analogRead(SENSOR_PIN);
+  }
+
+  sensorValueAvg /= 100; //get average value
+  float sensorVolts = sensorValueAvg / 1024 * 5.0;
+  Serial.println(F("Sensor value and volts:"))
+  Serial.println(sensorValueAvg);
+  Serial.println(sensorVolts);
+  return sensorValueAvg;
+}
+
+void radioSend(float value) {
+  memset(sendBuffer, 0, BUFFER_SIZE);
 
   dtostrf(value, 1, 2, sendBuffer);
   sendLength = strlen(sendBuffer);  // strlen will count length until first '\0' symbol
 
-  Serial.print("sending to node ");
+  Serial.print(F("Sending to node "));
   Serial.print(TONODEID, DEC);
-  Serial.print(" message [");
+  Serial.print(F(" message ["));
   Serial.print(sendBuffer);
-  Serial.print("]\n");
+  Serial.print(F("]\n"));
 
-  if (USEACK) {
-    if (radio.sendWithRetry(TONODEID, sendBuffer, sendLength))
-      Serial.println("ACK received!");
-    else
-      Serial.println("no ACK received");
-  } else  // don't use ACK
-  {
-    radio.send(TONODEID, sendBuffer, sendLength);
+#if (USEACK==true)
+  if (radio.sendWithRetry(TONODEID, sendBuffer, sendLength)) {
+    Serial.println("ACK received!");
+  } else {
+    Serial.println("no ACK received");
   }
+#else
+  radio.send(TONODEID, sendBuffer, sendLength);
+#endif
+  //digitalWrite(LAMP_PIN, HIGH);
+}
+
+void radioRecv(){
+  if (radio.receiveDone())
+  {
+    // The actual message is contained in the DATA array,
+    // and is DATALEN bytes in size:
+    Serial.print(F("Received from node "));
+    Serial.print(radio.SENDERID, DEC);
+    Serial.print(F(", message ["));
+
+    for (uint8_t i = 0; i < radio.DATALEN; i++){
+      Serial.print((char)radio.DATA[i]);
+    }
+    // something like command handler
+    if (!strcmp(radio.DATA, "read"))
+    {
+      radioSend(sensorValue);
+    }
+    // RSSI is the "Receive Signal Strength Indicator",
+    // smaller numbers mean higher power.
+    Serial.print(F("], RSSI "));
+    Serial.println(radio.RSSI);
+
+    // Send an ACK if requested.
+    // (You don't need this code if you're not using ACKs.)
+    if (radio.ACKRequested()) {
+      radio.sendACK();
+      Serial.println(F("ACK sent"));
+    }
+    blinkLed(LED_PIN, 10);
+  }
+}
+
+void setup() {
+  initSerial();
+  initRadio();
+
+  pinMode(LAMP_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  delay(20000); //wait to stabilize
+  startTime = millis();
+}
+
+void loop() {
+  radio.sendWithRetry(2, message_start, sizeof(message_start), 3, ACK_WAIT_TIME);
+  startTime = millis();
+  uint32_t sensorReadTime = millis();
+
+  while (millis() - startTime < LOOP_WAIT_TIME) {
+
+    if (millis() - sensorReadTime > READ_SENSOR_TIMEOUT) {
+      sensorValue = getSensorValueAvg();
+      sensorReadTime = millis();
+    }
+
+    // if sensor value is higher than threshold, send message
+    if (sensorValue > THRESHOLD_SENSOR_VAL) {
+      digitalWrite(LAMP_PIN, LOW);
+      radioSend(sensorValue);
+    } else {
+      digitalWrite(LAMP_PIN, HIGH);
+    }
+
+    radioRecv();
+  }
+  sleepForSeconds(SLEEP_TIME_SEC);
 }
